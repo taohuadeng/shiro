4ca1854924b11cbfd9fb8751b49bbd792511afd1
taofadeng <taofadeng@21tb.com>
2016-04-08 23:05:58 +0800

    shiro配置使用Java代码

--- shiro_01/src/test/java/com/thd/shiro/shiro01/LoginLogoutTest.java
+++ shiro_01/src/test/java/com/thd/shiro/shiro01/LoginLogoutTest.java
@@ -1,10 +1,11 @@
 package com.thd.shiro.shiro01;
 
+import org.apache.shiro.authc.*;
+import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.apache.shiro.util.ThreadContext;
@@ -47,7 +48,6 @@ public class LoginLogoutTest {
         subject.logout();
     }
 
-
     @Test
     public void testCustomRealm() {
         //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
@@ -75,6 +75,57 @@ public class LoginLogoutTest {
         //6、退出
         subject.logout();
     }
+
+    @Test
+    public void testCustomRealmTao() {
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(new Realm() {
+            @Override
+            public String getName() {
+                return "myrealm1";
+            }
+
+            @Override
+            public boolean supports(AuthenticationToken token) {
+                return token instanceof UsernamePasswordToken; //仅支持UsernamePasswordToken类型的Token
+            }
+
+            @Override
+            public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+                String username = (String) token.getPrincipal();  //得到用户名
+                String password = new String((char[]) token.getCredentials()); //得到密码
+                if (!"TaoHuaDeng".equals(username)) {
+                    throw new UnknownAccountException(); //如果用户名错误
+                }
+
+                if (!"123456".equals(password)) {
+                    throw new IncorrectCredentialsException(); //如果密码错误
+                }
+
+                System.out.println("MyRealm1 SimpleAuthenticationInfo");
+                //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+                return new SimpleAuthenticationInfo(username, password, getName());
+            }
+        });
+
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("TaoHuaDeng", "123456");
+
+        try {
+            // 登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            // 身份验证失败
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        //6、退出
+        subject.logout();
+    }
+
 
     @Test
     public void testCustomMultiRealm() {
--- shiro_01/src/test/java/com/thd/shiro/shiro01/realm/MyRealm1.java
+++ shiro_01/src/test/java/com/thd/shiro/shiro01/realm/MyRealm1.java
@@ -10,17 +10,17 @@ import org.apache.shiro.realm.Realm;
  */
 public class MyRealm1 implements Realm {
 
-    //@Override
+    @Override
     public String getName() {
         return "myrealm1";
     }
 
-    //@Override
+    @Override
     public boolean supports(AuthenticationToken token) {
         return token instanceof UsernamePasswordToken; //仅支持UsernamePasswordToken类型的Token
     }
 
-    //@Override
+    @Override
     public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
 
         String username = (String) token.getPrincipal();  //得到用户名
